2aa115c696ffa64d4a27307bb38f34e84dceeb9a
nico-0906 <nico1844@gmail.com>
2020-06-15 19:42:52 -0300

    Primer commit de prueba.

---
+++ prueba/src/prueba.c
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Name        : prueba.c
+ Author      : Nicolas
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	system("color 02");
+	int a;
+	printf("escribi un numero: ");
+	scanf("%d", &a);
+	printf("el numero es: %d\n",a);
+	system("pause");
+	return EXIT_SUCCESS;
+}
